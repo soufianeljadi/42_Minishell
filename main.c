9fe8f02a4f3ea63f8ea239bf2046ff1b1603d588
Soufiane El Jadi <sel-jadi@e2r4p2.1337.ma>
2024-03-10 20:13:40 +0000

    first

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#define MAX_COMMAND_LENGTH 100
+
+int main() {
+    char *command;
+
+    while (1) {
+
+        command = readline("bash: ");
+        if (!command) {
+            // Error or EOF while reading input
+            break;
+        }
+        
+        // Check for exit command
+        if (strcmp(command, "exit") == 0) {
+            printf("Exiting...\n");
+            free(command);
+            break;
+        }
+
+        // Execute the command
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+        } else if (pid == 0) {  // Child process
+            execlp(command, command, NULL);
+            perror("exec");
+            exit(EXIT_FAILURE);
+        } else {  // Parent process
+            waitpid(pid, NULL, 0);
+        }
+
+        free(command);
+    }
+
+    return 0;
+}
