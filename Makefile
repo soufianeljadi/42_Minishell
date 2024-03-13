fdede4a2b03ba817ab9d3e98342841c447a00d21
Salah-Eddine Diouane <sdiouane@e2r2p4.1337.ma>
2024-03-13 01:07:35 +0000

    git of garagir

---
+++ Makefile
@@ -0,0 +1,38 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: sdiouane <sdiouane@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/03/12 23:52:42 by sdiouane          #+#    #+#              #
+#    Updated: 2024/03/13 01:01:04 by sdiouane         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = minishell
+
+CC = cc #-g -fsanitize=address 
+CFLAGS = -Wall -Wextra -Werror 
+RDFLAGS = -lreadline
+SRC = main.c \
+      ft_split.c \
+	  lsts.c
+
+OBJ = $(SRC:.c=.o)
+
+$(NAME): $(OBJ)
+	@$(CC) $(OBJ) -o $(NAME) $(RDFLAGS)
+
+all: $(NAME)
+
+%.o : %.c minishell.h
+	@$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	@rm -rf $(OBJ)
+
+fclean: clean
+	@rm -rf $(NAME)
+
+re: fclean all
--- main.c
+++ main.c
@@ -1,45 +1,67 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdiouane <sdiouane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 23:52:10 by sdiouane          #+#    #+#             */
+/*   Updated: 2024/03/13 01:05:23 by sdiouane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#define MAX_COMMAND_LENGTH 100
+#include "minishell.h"
 
-int main() {
+int main()
+{
+    t_sep *cmd = NULL;
+    t_sep *tmp= NULL;
     char *command;
+    char **str;
+    int i = 0;
+    
+    while (1)
+    {
 
-    while (1) {
-
-        command = readline("bash: ");
+        command = readline("minishell :$ ");
         if (!command) {
             // Error or EOF while reading input
             break;
         }
-        
+        str = ft_split(command, ';');
+        tmp = cmd;
+        while (str[i])
+        {
+		    ft_lstadd_back(&cmd, ft_lstnew(str[i]));
+            i++;
+        }
+        cmd->next=NULL;
+        // printf("hgere\n");
+        while (tmp)
+        {
+            printf("%s\n", tmp->cmd_sep);
+            tmp = tmp->next;
+        }
+        // print_list(tmp);
         // Check for exit command
         if (strcmp(command, "exit") == 0) {
-            printf("Exiting...\n");
             free(command);
             break;
         }
 
         // Execute the command
-        pid_t pid = fork();
-        if (pid == -1) {
-            perror("fork");
-        } else if (pid == 0) {  // Child process
-            execlp(command, command, NULL);
-            perror("exec");
-            exit(EXIT_FAILURE);
-        } else {  // Parent process
-            waitpid(pid, NULL, 0);
-        }
+        // pid_t pid = fork();
+        // if (pid == -1) {
+        //     perror("fork");
+        // } else if (pid == 0) {  // Child process
+        //     execlp(command, command, NULL);
+        //     perror("exec");
+        //     exit(EXIT_FAILURE);
+        // } else {  // Parent process
+        //     waitpid(pid, NULL, 0);
+        // }
 
         free(command);
     }
-
     return 0;
 }
