75f632836b51a4ddb870dbe8834ca897e9805af7
Soufiane El Jadi <sel-jadi@e1r5p1.1337.ma>
2024-03-13 13:03:05 +0000

    trash

--- main.c
+++ main.c
@@ -8,38 +8,50 @@
 
 #define MAX_COMMAND_LENGTH 100
 
-int main() {
-    char *command;
+int main(int arc, char **arv, char **env)
+{
+    int i = 0;
 
-    while (1) {
-
-        command = readline("bash: ");
-        if (!command) {
-            // Error or EOF while reading input
-            break;
-        }
-        
-        // Check for exit command
-        if (strcmp(command, "exit") == 0) {
-            printf("Exiting...\n");
-            free(command);
-            break;
-        }
-
-        // Execute the command
-        pid_t pid = fork();
-        if (pid == -1) {
-            perror("fork");
-        } else if (pid == 0) {  // Child process
-            execlp(command, command, NULL);
-            perror("exec");
-            exit(EXIT_FAILURE);
-        } else {  // Parent process
-            waitpid(pid, NULL, 0);
-        }
-
-        free(command);
+    while (env[i])
+    {
+        printf("%s\n", env[i]);
+        i++;
     }
-
-    return 0;
 }
+
+
+// int main() {
+//     char *command;
+
+//     while (1) {
+
+//         command = readline("bash: ");
+//         if (!command) {
+//             // Error or EOF while reading input
+//             break;
+//         }
+        
+//         // Check for exit command
+//         if (strcmp(command, "exit") == 0) {
+//             printf("Exiting...\n");
+//             free(command);
+//             break;
+//         }
+
+//         // Execute the command
+//         pid_t pid = fork();
+//         if (pid == -1) {
+//             perror("fork");
+//         } else if (pid == 0) {  // Child process
+//             execlp(command, command, NULL);
+//             perror("exec");
+//             exit(EXIT_FAILURE);
+//         } else {  // Parent process
+//             waitpid(pid, NULL, 0);
+//         }
+
+//         free(command);
+//     }
+
+//     return 0;
+// }
