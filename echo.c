402efe2c393a50b087a7484681ec70516efa144e
Salah-Eddine Diouane <sdiouane@e1r9p1.1337.ma>
2024-03-23 17:40:45 +0000

    update dyl echo

--- echo.c
+++ echo.c
@@ -6,7 +6,7 @@
 /*   By: sdiouane <sdiouane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 16:50:26 by sdiouane          #+#    #+#             */
-/*   Updated: 2024/03/23 17:04:38 by sdiouane         ###   ########.fr       */
+/*   Updated: 2024/03/23 17:40:02 by sdiouane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,87 @@
 
 void echo_fct(char *line)
 {
-    int i = 0;
-    int newline = 1;
-    int in_quotes = 0;
+	int i = 0;
+	int newline = 1;
+	int in_quotes = 0;
 
-    while (line[i] == ' ' || line[i] == '\t')
-        i++;
-    if (strncmp(&line[i], "echo", 4) == 0)
+	while (line[i] == ' ' || line[i] == '\t')
+		i++;
+	if (strncmp(&line[i], "echo", 4) == 0)
 	{
-        i = i + 4;
-        while (line[i] == ' ' || line[i] == '\t')
-            i++;
-        if (line[i] == '"' || line[i] == '\'')
+		i = i + 4;
+		while (line[i] == ' ' || line[i] == '\t')
+			i++;
+		if (line[i] == '"' || line[i] == '\'')
 		{
-            in_quotes = 1;
-            i++;
-        }
-        if (strncmp(&line[i], "-n", 2) == 0)
+			in_quotes = 1;
+			i++;
+		}
+		if (strncmp(&line[i], "-n", 2) == 0)
 		{
-            newline = 0;
-            i += 2;
-            while (line[i] == ' ' || line[i] == '\t')
-                i++;
-        }
-        if (in_quotes)
+			i += 2;
+			if (line[i] == ' ' || line[i] == '\t')
+				newline = 0;
+			while (line[i] == ' ' || line[i] == '\t')
+				i++;
+		}
+		if (in_quotes)
 		{
-            while (line[i] != '"' && line[i] != '\'' && line[i] != '\0')
+			while (line[i] != '"' && line[i] != '\'' && line[i] != '\0')
 			{
-                printf("%c", line[i]);
-                i++;
-            }
-            if (line[i] == '"' || line[i] != '\'')
-                i++;
-        }
+				printf("%c", line[i]);
+				i++;
+			}
+			if (line[i] == '"' || line[i] != '\'')
+				i++;
+		}
 		else
-            printf("%s", &line[i]);
-        }
-        if (newline)
-            printf("\n");
+			printf("%s", &line[i]);
+		}
+		if (newline)
+			printf("\n");
 }
+
+
+// void echo_fct(char *line) {
+//     int i = 0;
+//     int in_quotes = 0;
+//     int num_n_flags = 0; // Compteur pour les occurrences de l'option -n
+
+//     while (line[i] == ' ' || line[i] == '\t')
+//         i++;
+//     if (strncmp(&line[i], "echo", 4) == 0) {
+//         i = i + 4;
+//         while (line[i] == ' ' || line[i] == '\t')
+//             i++;
+//         if (line[i] == '"' || line[i] == '\'') {
+//             in_quotes = 1;
+//             i++;
+//         }
+//         while (strncmp(&line[i], "-n", 2) == 0) {
+//             num_n_flags++; // Incrémente le compteur d'occurrences de -n
+//             i += 2;
+//             // Ignorer les espaces supplémentaires entre les options -n
+//             while (line[i] == ' ' || line[i] == '\t')
+//                 i++;
+//         }
+//         if (in_quotes) {
+//             while (line[i] != '"' && line[i] != '\'' && line[i] != '\0') {
+//                 printf("%c", line[i]);
+//                 i++;
+//             }
+//             if (line[i] == '"' || line[i] != '\'')
+//                 i++;
+//         } else {
+//             // Imprime les caractères après la dernière occurrence de -n
+//             while (line[i] != '\0') {
+//                 printf("%c", line[i]);
+//                 i++;
+//             }
+//         }
+
+//         // Print newline seulement si aucune occurrence de -n n'a été trouvée
+//         if (num_n_flags == 0)
+//             printf("\n");
+//     }
+// }
