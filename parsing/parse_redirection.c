e4bdfabaf4c0928d2f2a0e2c4c00edef8cd006ee
Soufiane El Jadi <sel-jadi@e1r6p5.1337.ma>
2024-05-18 21:05:17 +0100

    fix SEGV in redirections

--- parsing/parse_redirection.c
+++ parsing/parse_redirection.c
@@ -3,116 +3,66 @@
 /*                                                        :::      ::::::::   */
 /*   parse_redirection.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sdiouane <sdiouane@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sel-jadi <sel-jadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 23:09:54 by sdiouane          #+#    #+#             */
-/*   Updated: 2024/05/17 15:00:02 by sdiouane         ###   ########.fr       */
+/*   Updated: 2024/05/18 21:03:58 by sel-jadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static	int	only_spaces__(char *line, int i)
+static int only_spaces__(char *line, int i)
 {
-	int	r;
-
-	r = 0;
-	while (line[i] == ' ' || line[i] == '\t')
+	while (line[i] != '\0' && (line[i] == ' ' || line[i] == '\t'))
 		i++;
 	if (line[i] == '\0' || line[i] == '|')
-		r = 1;
-	return (r);
+		return 1;
+	return 0;
 }
 
-static	void	        check_line(char *line, int i, int r)
+static void check_line(char *line, int i, int *r)
 {
 	while (line[i] != '\0')
 	{
-		if ((line[i] == '>' && line[i + 1] == '>') || (line[i] == '<' && line[i + 1] == '<'))
+		if (line[i + 1] != '\0' && ((line[i] == '>' && line[i + 1] == '>') || (line[i] == '<' && line[i + 1] == '<')))
 		{
-			if (line[i + 2] == '<' || line[i + 2] == '>')
+			if (line[i + 2] != '\0' && (line[i + 2] == '<' || line[i + 2] == '>'))
 			{
-				r = 1;
-				break ;
+				*r = 1;
+				break;
 			}
-			i = i + 2;
-			r = only_spaces__(line, i);
+			i += 2;
+			*r = only_spaces__(line, i);
+			if (*r)
+				break;
 		}
 		else if (line[i] == '>' || line[i] == '<')
 		{
-			if ((line[i] == '>' && line[i + 1] == '\0') || (line[i] == '<' && line[i + 1] == '\0'))
+			if (line[i + 1] == '\0')
 			{
-				r = 1;
-				break ;
+				*r = 1;
+				break;
 			}
-			r = only_spaces__(line, i + 1);
+			*r = only_spaces__(line, i + 1);
+			if (*r)
+				break;
 		}
 		i++;
 	}
 }
 
-int	parse_redirection(char *line)
+int parse_redirection(char *line)
 {
-	int	i;
-	int	r;
-
+	int i;
+	int r;
+	
 	i = 0;
 	r = 0;
-	while (line[i] <= 32)
+	while (line[i] != '\0' && line[i] <= 32)
 		i++;
-	while (line[i] != '\0')
-	{
-		check_line(line, i, r);
-		i++;
-	}
+	check_line(line, i, &r);
 	if (r == 0)
-		return (0);
-	return (1);
+		return 0;
+	return 1;
 }
-
-
-// static void check_line(char *line, int i, int *r)
-// {
-//     while (line[i] != '\0')
-//     {
-//         if ((line[i] == '>' && line[i + 1] == '>') || (line[i] == '<' && line[i + 1] == '<'))
-//         {
-//             if (line[i + 2] == '<' || line[i + 2] == '>')
-//             {
-//                 *r = 1;
-//                 break;
-//             }
-//             i = i + 2;
-//             *r = only_spaces__(line, i);
-//         }
-//         else if (line[i] == '>' || line[i] == '<')
-//         {
-//             if ((line[i] == '>' && line[i + 1] == '\0') || (line[i] == '<' && line[i + 1] == '\0'))
-//             {
-//                 *r = 1;
-//                 break;
-//             }
-//             *r = only_spaces__(line, i + 1);
-//         }
-//         i++;
-//     }
-// }
-
-// int parse_redirection(char *line)
-// {
-//     int i;
-//     int r;
-
-//     i = 0;
-//     r = 0;
-//     while (line[i] <= 32)
-//         i++;
-//     while (line[i] != '\0')
-//     {
-//         check_line(line, i, &r);
-//         i++;
-//     }
-//     if (r == 0)
-//         return (0);
-//     return (1);
-// }
--- parsing/parsing.c
+++ parsing/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sdiouane <sdiouane@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sel-jadi <sel-jadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 15:55:13 by sdiouane          #+#    #+#             */
-/*   Updated: 2024/05/17 16:22:47 by sdiouane         ###   ########.fr       */
+/*   Updated: 2024/05/18 21:04:10 by sel-jadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,7 +73,7 @@ int	parsing(char *line)
 		return (1);
 	if (ft_quots(&line))
 		return (1);
-	// if (parse_redirection(line))
-	// 	return (1);
+	if (parse_redirection(line))
+		return (1);
 	return (0);
 }
